b35660e5b053fa414eab4fa6fa90eba4b04baa45
Mykhaylo_Mikus <olga.lavinska@thomsonreuters.com>
2016-03-31 12:07:08 +0300

    @Repository

--- src/main/java/com/galprom/repository/GreedRepository.java
+++ src/main/java/com/galprom/repository/GreedRepository.java
@@ -2,7 +2,9 @@ package com.galprom.repository;
 
 import com.galprom.model.Greed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GreedRepository extends JpaRepository<Greed,Long> {
 
 }
